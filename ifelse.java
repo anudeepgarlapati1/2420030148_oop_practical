c8ce126a3a119d322f4ea3daa12e1129482c334e
anudeepgarlapati1 <anudeepgarlapati@gmail.com>
2025-08-02 09:06:21 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+package myproject;
+import java.util.*;
+public class ifelse {
+      public static void main(String args[]) {
+         {
+	int num = 7;
+	if (num%2==0) {
+	   System.out.println("Even number");
+	 } else {
+	    System.out.println("Odd number");
+	 }
+      }
+  }
